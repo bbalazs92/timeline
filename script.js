e7b86a2bf5910e6601ed1c1bcfb7722df8012daa
bbalazs92 <balazs.birta@gmail.com>
2025-05-29 14:37:24 +0200

    implemented mouse position calculation within timeline

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const timeline = document.getElementById('timeline');
 const tooltip = document.getElementById('tooltip');
 
-const startDate = new Date('2025-01-01');
+const startDate = new Date('2025-01-01');                                   //rewrite to use temporal!!
 
 const daysInYear = 365;
+
+timeline.addEventListener('mousemove', (e) => {
+    //mouse position calculation
+    const timelineRectangle = timeline.getBoundingClientRect();             // getBoundingClientRect() returns a DOMRect object 
+    const mouseXPosition = e.clientX - timelineRectangle.left;              // mouse X position relative to timeline
+    const timelineWidth = timelineRectangle.width;
+});
