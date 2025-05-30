e83321bd683dacc26b0e1b064c6ffc85c6d8d3e9
bbalazs92 <balazs.birta@gmail.com>
2025-05-30 17:14:34 +0200

    added eventlistener to display content on timeline click based on the current date

--- script.js
+++ script.js
@@ -31,3 +31,24 @@ timeline.addEventListener('mousemove', (e) => {
 timeline.addEventListener('mouseleave', () => {
     tooltip.style.display = 'none';
 });
+
+timeline.addEventListener('click', (e) => {
+
+    //mouse position calculation
+    const timelineRectangle = timeline.getBoundingClientRect();             // getBoundingClientRect() returns a DOMRect object 
+    const mouseXPosition    = e.clientX - timelineRectangle.left;           // mouse X position relative to timeline
+    const timelineWidth     = timelineRectangle.width;
+
+    //date calculation
+    const dayIndex    = Math.floor((mouseXPosition / timelineWidth) * daysInYear);       // calculate cursor position 0 - 364
+    const currentDate = new Date(startDate);                                
+    currentDate.setDate(currentDate.getDate() + dayIndex);                  // setDate() changes value in place
+
+    //format date
+    const year  = String(currentDate.getFullYear()).slice(-2);              // last 2 digits of the year
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');      // months are 0 indexed
+    const day   = String(currentDate.getDate()).padStart(2, '0');
+
+    articles.innerText = `${year}/${month}/${day}`;
+    articles.style.height = '50px';
+});
